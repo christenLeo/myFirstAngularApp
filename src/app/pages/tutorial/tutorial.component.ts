7161d53a21bc06462269ba52b9e14bba829e1de9
Leona Christen <leo.christenmacedo@gmail.com>
2023-11-25 08:11:56 -0300

    chore: adds tutorial page

---
+++ src/app/pages/tutorial/tutorial.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tutorial',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tutorial.component.html',
+  styleUrl: './tutorial.component.css'
+})
+export class TutorialComponent {
+
+}
