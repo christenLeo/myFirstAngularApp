f2f7978b812ffa829b80f00f59e5a87edba0db1b
Leona Christen <leo.christenmacedo@gmail.com>
2023-11-25 08:10:15 -0300

    chore: initial setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { TutorialComponent } from './pages/tutorial/tutorial.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent, children: [
+    { path: 'tutorial', component: TutorialComponent }
+  ]},
+];
