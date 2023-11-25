fe5afaf7676418d04957ad87544d084484e2e483
Leona Christen <leo.christenmacedo@gmail.com>
2023-11-25 12:31:33 -0300

    chore: calls userService in home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,14 +2,31 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../components/header/header.component';
 import { TutorialComponent } from '../tutorial/tutorial.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { UserService } from '../../services/user/user.service';
+import { User } from '../../models/user/user';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, TutorialComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    TutorialComponent,
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  constructor (
+    private userService: UserService
+  ) {}
+  users: User[] = [];
 
+  ngOnInit(): void {
+    this.userService.getAllUsers().subscribe(array => this.users = array)
+  }
 }
